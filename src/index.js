34494aafecda2c25da34382664bab768440040a1
Parvej Ansari <116214778+lokeyparry@users.noreply.github.com>
2024-03-21 08:37:08 +0530

    hhh

---
+++ src/index.js
@@ -0,0 +1,47 @@
+// require('dotenv').config({ path: './env' })
+
+
+import connectDB from "./db/index.js"
+import dotenv from "dotenv"
+
+
+
+
+dotenv.config({
+    path: "./env"
+})
+
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`server is running on port : ${process.env.PORT}`);
+        })
+    })
+    .catch((err) => {
+        console.log("mongo db connection failed!!", err);
+    })
+
+
+
+
+
+// import mongoose from "mongoose";
+// import express from "express";
+// import { DB_NAME } from "./constants.js"
+// const app = express()
+
+// (async() => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("error", (error) => {
+//             console.log("ERROR: ", error);
+//             throw error;
+//         })
+//         app.listen(process.env.PORT, () => {
+//             console.log(`App is listening on port:  ${process.env.PORT}`);
+//         })
+//     } catch (error) {
+//         console.log("error", error);
+//         throw error;
+//     }
+// })()
